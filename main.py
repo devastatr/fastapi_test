5650d7f6d55d1c57706cef6e9438512de9c7bdcd
Alk <test@mail.local>
2024-06-29 00:57:01 +0300

    add add_task

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from typing import Optional
-from fastapi import FastAPI
+from typing import Optional, Annotated
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Task(BaseModel):
+class STaskAdd(BaseModel):
     name: str
     deck: Optional[str] = None
 
 
-@app.get("/tasks")
-def get_tasks():
-    task = Task(name="Тестовое задание")
-    return {"data": task}
+class STask(STaskAdd):
+    id: int
+
+
+tasks = []
+
+
+@app.post("/tasks")
+async def add_task(
+        task: Annotated[STaskAdd, Depends()],
+):
+    tasks.append(task)
+    return {"ok": True}
+
+# @app.get("/tasks")
+# def get_tasks():
+#     task = Task(name="Тестовое задание")
+#     return {"data": task}
